f26be2a12697b61e797a50073ec7400b7c3d8578
ianad <ian.dalton@ucf.edu>
2018-11-28 13:33:24 -0500

    move to requirements.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
-    name="LLRN Basic Python Distribution",
-    version="0.1.0",
-    install_requires=[
-        "pandas",
-        "numpy",
-        "jupyter",
-        "xlrd",
-        "requests",
-        "seaborn",
-        "matplotlib"
-    ],
+    name = "LLRN Standard Python Distribution",
+    version = "0.1.0",
+    install_requires = required
     # ...
 )
